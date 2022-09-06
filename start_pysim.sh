f0c10bd57fb7d97aadf944988547f8a3dfcb2d66
ingoahrns <ingo@ahrns.de>
2022-09-06 19:57:05 +0200

    Signed-off-by: ingoahrns <ingo@ahrns.de>

--- start_pysim.sh
+++ start_pysim.sh
@@ -8,19 +8,20 @@ echo ' '
 
 kill.sh
 
-gnome-terminal --working-directory=/home/pi --window-with-profile=ros --hide-menubar --title="Arduino Controller" --geometry=73x28+0+20 -- ~/robot/scripts/start_simarduino.sh 
+# gnome-terminal --working-directory=/home/pi --window-with-profile=ros --hide-menubar --title="Arduino Controller" --geometry=73x28+0+20 -- ~/robot/scripts/start_simarduino.sh 
 
 # gnome-terminal --working-directory=/home/pi --window-with-profile=ros --hide-menubar --title="Joystick Controller" --geometry=73x28+600+20 -- ~/robot/scripts/start_joystick.sh
 
 # gnome-terminal --working-directory=/home/pi --window-with-profile=ros --hide-menubar --title="Robot Joystick Control" --geometry=73x28+1200+20 -- ~/robot/scripts/start_joyctrl.sh 
 
-gnome-terminal --working-directory=/home/pi --window-with-profile=ros --hide-menubar --title="Camera Control" --geometry=73x28+0+520 -- ~/robot/scripts/start_simcam.sh
+# gnome-terminal --working-directory=/home/pi --window-with-profile=ros --hide-menubar --title="Camera Control" --geometry=73x28+0+520 -- ~/robot/scripts/start_simcam.sh
 
 gnome-terminal --working-directory=/home/pi --window-with-profile=ros --hide-menubar --title="LIDAR Control" --geometry=73x28+600+520 -- ~/robot/scripts/start_simlidar.sh 
 
-gnome-terminal --working-directory=/home/pi --window-with-profile=ros --hide-menubar --title="Ball Detector" --geometry=73x28+1200+520 -- ~/robot/scripts/start_balldetect.sh 
+# gnome-terminal --working-directory=/home/pi --window-with-profile=ros --hide-menubar --title="Ball Detector" --geometry=73x28+1200+520 -- ~/robot/scripts/start_balldetect.sh 
 
 gnome-terminal --working-directory=/home/pi --window-with-profile=ros --hide-menubar --title="Robot Controller" --geometry=73x28+0+20 -- ~/robot/scripts/start_pyrobctrl.sh 
 
 gnome-terminal --working-directory=/home/pi --window-with-profile=ros --hide-menubar --title="Navigation Controller" --geometry=73x28+600+20 -- ~/robot/scripts/start_pynav.sh
+# gnome-terminal --working-directory=/home/pi --window-with-profile=ros --hide-menubar --title="Navigation Controller" --geometry=73x28+600+20 -- ~/robot/scripts/start_navigation.sh
 
